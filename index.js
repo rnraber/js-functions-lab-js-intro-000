c2848008c277178cec53db9ac3b19dfcfc43d5bd
Ryann Raber <ryann.raber@yahoo.com>
2019-11-01 01:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return 'It\'s ${days} days until ${holiday}!'
+  return 'It\'s' + days + 'days until' + holiday + '!'
 }
-
-var name='you';
-happyHolidaysTo();
